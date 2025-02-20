a4d1c9aa0ef342768e0a687dc6d66b762058a0f1
Jean Pierre <jeanpierrendagije@outlook.com>
2025-02-20 11:45:34 +0100

    contact form

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -5,9 +5,57 @@ jQuery(document).ready(function($) {
   $('form.contactForm').submit(function(event) {
     event.preventDefault(); // Prevent the default form submission
 
-    var form = $(this);
-    var url = form.attr('action');
+    var form = $(this); // Current form
+    var url = form.attr('action'); // Formspree or other endpoint
 
+    // Validate form fields before submission
+    var ferror = false;
+    var emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+
+    form.find('input, textarea').each(function() {
+      var i = $(this); // Current input/textarea
+      var rule = i.attr('data-rule');
+
+      if (rule !== undefined) {
+        var ierror = false; // Error flag for current input
+        var pos = rule.indexOf(':', 0);
+        var exp = pos >= 0 ? rule.substr(pos + 1) : '';
+
+        switch (rule.split(':')[0]) {
+          case 'required':
+            if (i.val() === '') {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'minlen':
+            if (i.val().length < parseInt(exp)) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'email':
+            if (!emailExp.test(i.val())) {
+              ferror = ierror = true;
+            }
+            break;
+
+          case 'regexp':
+            if (!new RegExp(exp).test(i.val())) {
+              ferror = ierror = true;
+            }
+            break;
+        }
+
+        // Display validation message
+        i.next('.validation').html((ierror ? (i.attr('data-msg') || 'Invalid input') : '')).show('blind');
+      }
+    });
+
+    // If there are validation errors, stop submission
+    if (ferror) return false;
+
+    // Submit form via AJAX
     $.ajax({
       type: "POST",
       url: url,
@@ -18,8 +66,10 @@ jQuery(document).ready(function($) {
         $("#sendmessage").addClass("show");
         $("#errormessage").removeClass("show");
 
-        // Clear the form fields
-        form[0].reset(); // This resets the form
+        // Manually clear each field
+        form.find('input[type="text"]').val(''); // Clear text inputs
+        form.find('input[type="email"]').val(''); // Clear email input
+        form.find('textarea').val(''); // Clear textarea
 
         // Optionally, clear validation messages
         form.find('.validation').html('').hide();
@@ -31,5 +81,7 @@ jQuery(document).ready(function($) {
         $('#errormessage').html("Oops! There was an error. Please try again.");
       }
     });
+
+    return false; // Prevent default form submission
   });
 });
